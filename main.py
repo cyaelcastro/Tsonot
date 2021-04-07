a39ce4e04655021915845b8f2f321db52464611f
Yael Castro <cyaelcc94@gmail.com>
2021-04-06 23:31:42 -0500

    Give main.py code a cat's hand
    Comments about the execution and logic will be added

--- main.py
+++ main.py
@@ -18,8 +18,11 @@ BASE_LOCATION = "./media/"
 #Flag to show the log in the MQTT connection
 LOG_ACTIVATED = 0
 
+#Global variable to record the last executed command
 command_executed = []
 
+
+#Check Python version, requires Python 3.x
 def check_version():
     print("Checking Python version")
     if sys.version_info.major < 3:
@@ -27,29 +30,42 @@ def check_version():
     print("Python version: {0}.{1} OK".format(sys.version_info.major, sys.version_info.minor))
 
 
+#Obtain the host OS
 def check_os():
     print("OS: {0}".format(platform.system()))
     return platform.system()
 
 
+#Load command from commands.json based on the host OS
 def load_commands(os):
     with open("commands.json") as json_file:
         commands = json.load(json_file)
         return commands.get(os)
 
 
+#Integrate the command list from the MQTT Topics
 def generate_command(mqtt_topic, command_json):
+
+    #Split the topics to get the filename to be executed
     mqtt_topic_levels = mqtt_topic.split("/")
 
+    #Check if the file exist in the folder
     if check_file_exist(mqtt_topic_levels[4]):
+        
+        #Get the command_list from command.json matching the action received in MQTT topic
         command_list = command_json.get(mqtt_topic_levels[2])
+        #Append the file name in the command_list
         command_list.append(mqtt_topic_levels[4])
+        #Obtain the file's absolute path
         command_list[-1] = Path(BASE_LOCATION+command_list[-1]).absolute()
+        #Obtain the list "kill_pid" from command.json 
         kill_command = command_json.get("kill_pid")
+        #Execute the command from the generated lists
         run_command(command_list, kill_command)
 
 
 def check_file_exist(file_name):
+    #Check if the received file exist in the folder
     return Path(BASE_LOCATION+file_name).exists()
 
 
@@ -102,23 +118,34 @@ def execute_kill_command(command_list, kill_command):
 
 
 def create_mqtt_conection(commands):
+    #Create MQTT conection adding the client and initializing the userdata
     client = mqtt.Client(client_id="Tsonot", userdata=commands)
+
+    #Assinging the on_connect Callback
     client.on_connect=on_connect
+
+    #In case the LOG_ACTIVATED is True, the execution log will be shown
     if LOG_ACTIVATED:
         client.on_log=on_log
     
+    #Catch error in the connection with the broker
     try:
         client.connect(MQTT_BROKER)
     except TimeoutError:
         print("Error in connection with broker")
         exit(1)
-        
+    
+    #Assigning the on_message Callback
     client.on_message=on_message
+
+    #Subscribe to topics: Browser, picture, video and kill commands
     client.subscribe(MQTT_TOPICS)
     return client
 
 
+#On connect Callback
 def on_connect(client, userdata, flags, rc):
+    #Connection return code
     if rc==0:
         client.connected_flag=True
         print("Connection OK")
@@ -126,13 +153,15 @@ def on_connect(client, userdata, flags, rc):
 
 def on_message(client, userdata, message):
     
+    #Topic of killing a process
     if message.topic == MQTT_TOPICS[3][0]:
         pass
+    #Topics of executing an application
     else:
         generate_command(message.topic, userdata)
     
-    
 
+#Logging Callback 
 def on_log(client, userdata, level, buf):
     print("log: ",buf)
 
@@ -143,9 +172,12 @@ if __name__  == "__main__":
     #Check OS to know how to handle system calls
     os = check_os()
 
+    #Load the commands from file based on the host OS 
     commands = load_commands(os)
 
+    #Create the MQTT conection sending the commands as userdata
     mqtt_client = create_mqtt_conection(commands)
+
     mqtt_client.loop_forever()
     
     
